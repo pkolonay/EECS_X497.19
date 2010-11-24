1e9953b4fc7f8f2110ad8857c12fd1a9da791314
pkolonay <pkolonay@gmail.com>
2010-11-24 12:25:45 -0500

    Added comments.

--- LABS/lab3c/drvcore.c
+++ LABS/lab3c/drvcore.c
@@ -1,6 +1,13 @@
 #include "..\inc\atmega2560.h"
 #include "drvcore.h"
 
+/**
+  * Function: myitoa
+  * Description: Convert 16-bit interger value to ascii string.
+  * Parameters: myascii - array of ascii values.
+  *             length -  length of ascii string.
+  *             value - integer number to be converted
+  */
 void myitoa(UINT8 myascii[],UINT16 length, UINT16 value) {
  
 	volatile UINT8 i,j;
@@ -8,7 +15,8 @@ void myitoa(UINT8 myascii[],UINT16 length, UINT16 value) {
 
 	divisor = value;
 
-
+    /* work from highest power down to uints */
+	/* but place characters in array in the opposite order */
     for(i=0;i<length-1;i++) {
     	dividend = 1;
 	    for(j=1;j<=(length-i-1);j++) dividend *= 10;
